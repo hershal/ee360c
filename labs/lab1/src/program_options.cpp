2dcbf906c4e893dee8440e4b414cb6bfbf9c89d5
Hershal Bhave <hershal.bhave@gmail.com>
2014-09-27 02:16:52 -0500

    program options takes in input files now

--- labs/lab1/src/program_options.cpp
+++ labs/lab1/src/program_options.cpp
@@ -1,33 +1,53 @@
 #include <iostream>
 #include "boost/program_options.hpp"
+#include <iostream>
+#include <fstream>
+#include <iterator>
 
 #include "program_options.hpp"
 
+template<class T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
+{
+    copy(v.begin(), v.end(), std::ostream_iterator<T>(os, " ")); 
+    return os;
+}
+
 namespace po = boost::program_options;
 
+const char* program_options::kPOInputFileCmdline = "input-file,i";
+const char* program_options::kPOIntputFileDesc = "input file(s)";
+const char* program_options::kPOHelpCmdline = "help,h";
+const char* program_options::kPOHelpDesc = "produce this help message";
+
 program_options::program_options(int ac, char** av) {
+
+
     process_options(ac, av);
 }
 
 auto program_options::process_options(int ac, char** av) -> void {
     po::options_description desc("Allowed options");
     desc.add_options()
-        ("help", "produce help message")
-        ("compression", po::value<int>(), "set compression level");
+        ("input-file,i", po::value<std::vector<std::string> >(), "input files")
+        ("help", "help description");
     
+    /* Add the "input-file" flag as the implicit option */
+    po::positional_options_description p;
+    p.add("input-file", -1);
+
     po::variables_map vm;
-    po::store(po::parse_command_line(ac, av, desc), vm);
-    po::notify(vm);    
+    po::store(po::command_line_parser(ac, av).options(desc).positional(p).run(), vm);
+    po::notify(vm);
 
     if (vm.count("help")) {
         std::cout << desc << "\n";
         return;
     }
 
-    if (vm.count("compression")) {
-        std::cout << "Compression level was set to " 
-             << vm["compression"].as<int>() << ".\n";
+    if (vm.count("input-file")) {
+        input_files = vm["input-file"].as< std::vector<std::string> >();
     } else {
-        std::cout << "Compression level was not set.\n";
+        std::cout << desc << "\n";
     }
 }
--- labs/lab1/src/program_options.hpp
+++ labs/lab1/src/program_options.hpp
@@ -1,11 +1,25 @@
-#ifndef program_options_h
-#define program_options_h
+#ifndef PROGRAM_OPTIONS_H
+#define PROGRAM_OPTIONS_H
+
+#include <vector>
 
 class program_options {
 public:
     program_options(int ac, char** av);
+    auto get_input_files() ->  std::vector<std::string>;
+    auto get_output_files() -> std::vector<std::string>;
 private:
     auto process_options(int ac, char** av) -> void;
+    auto file_does_exist(std::string path) -> bool;
+    auto trim_file_extension(std::string* extension) -> void;
+
+    std::vector<std::string> input_files;
+    std::vector<std::string> output_files;
+
+    const static char* kPOInputFileCmdline;
+    const static char* kPOIntputFileDesc;
+    const static char* kPOHelpCmdline;
+    const static char* kPOHelpDesc;
 };
 
-#endif // program_options_h
+#endif // PROGRAM_OPTIONS_H
