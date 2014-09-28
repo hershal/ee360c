eab19e4e0e709e91f42909e998f548c05e530364
Hershal Bhave <hershal.bhave@gmail.com>
2014-09-28 02:11:51 -0500

    minor output changes

--- labs/lab1/src/lab1.cpp
+++ labs/lab1/src/lab1.cpp
@@ -5,14 +5,12 @@
 
 auto main(int ac, char** av) -> int {
 
-    std::cout << "main\n";
+    std::cout << "parsing options\n";
     auto options = program_options(ac, av);
 
+    std::cout << "processing files\n";
     for (const auto file : options.get_input_output_file_map()) {
-        std::cout << file.first << " -> " << file.second << "\n" << std::endl;
-    }
-
-    for (const auto file : options.get_input_output_file_map()) {
+        std::cout << file.first << " -> " << file.second << std::endl;
         complex_bipartite_graph cbg = complex_bipartite_graph();
         cbg.generate_data_structures(file.first);
     }
