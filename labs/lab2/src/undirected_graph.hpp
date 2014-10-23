fb16260762e45ea61225b87017bacbe91ca49c5f
Hershal Bhave <hershal.bhave@gmail.com>
2014-10-23 02:12:54 -0500

    removed graph_query from undirected_graph

--- labs/lab2/src/undirected_graph.hpp
+++ labs/lab2/src/undirected_graph.hpp
@@ -4,7 +4,6 @@
 #define UNDIRECTED_GRAPH_H
 
 #include "node.hpp"
-#include "graph_query.hpp"
 
 #include <map>
 #include <vector>
@@ -14,7 +13,7 @@
 
 class undirected_graph {
 public:
-    undirected_graph(size_t num_tics, size_t num_tacs);
+    undirected_graph(size_t num_nodes);
     auto add_connection(size_t id0, size_t id1, uint32_t comm_time) -> void;
     auto add_node(size_t id) -> void;
     auto to_string() const -> const std::string;
@@ -23,7 +22,6 @@ private:
     auto reset_nodes() -> void;
 
     std::vector<std::shared_ptr<node> > nodes;
-    graph_query query;
 };
 
 #endif  /* UNDIRECTED_GRAPH_H */
